ed560d5eb56fb306e429b0098734e6626044a1a4
Akbar Sami <sami@kali.com>
2025-01-22 21:39:23 +0600

    LongestPalindromicSubstring.py added

---
+++ 5. LongestPalindromicSubstring.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        res=''
+        resLen=0
+
+        for i in range(len(s)):
+            #for odd length
+            l,r=i,i
+            while l>=0 and r<len(s) and s[l] == s[r]:
+                if(r-l+1)>resLen:
+                    res=s[l:r+1]
+                    resLen=r-l+1
+                l-=1
+                r+=1
+            
+            #for even length
+            l,r=i,i+1
+            while l>=0 and r<len(s) and s[l] == s[r]:
+                if(r-l+1)>resLen:
+                    res=s[l:r+1]
+                    resLen=r-l+1
+                l-=1
+                r+=1
+        return res
+obj=Solution()
+print(obj.longestPalindrome('babad'))
