701d5115165446877caae278ce475227ea8f933d
Akbar Sami <sami@kali.com>
2025-02-10 19:36:49 +0600

    Candy.py added

---
+++ Candy.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def candy(self, ratings):
+        ans=[1]*len(ratings)
+
+        for i in range(1,len(ratings)):
+            if ratings[i]>ratings[i-1]:
+                ans[i]=ans[i-1]+1
+
+        for i in range(len(ratings)-2,-1,-1):
+            if ratings[i]>ratings[i+1]:
+                ans[i]=max(ans[i],ans[i+1]+1)
+        
+        return sum(ans)
+obj=Solution()
+print(obj.candy([1,0,2]))
