118282567f616a89f7b2a146e75677350b07eec4
Akbar Sami <sami@kali.com>
2025-02-13 22:23:57 +0600

    Combination.py added

---
+++ Combination.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def combine(self, n, k):
+        res=[]
+        def backtrack(start,comb):
+            if len(comb)==k:
+                res.append(comb.copy())
+                return
+            
+            for i in range(start,n+1):
+                comb.append(i)
+                backtrack(i+1,comb)
+                comb.pop()
+        backtrack(1,[])
+        return res
+obj=Solution()
+print(obj.combine(4,2))
+
+        
